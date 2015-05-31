36988f48abd133f5b26fe57bc565ba41ec3faf71
Drew <drew@finstrom.us>
2015-05-30 19:49:14 -0700

    add js linting via eslint and update script.js based on lenter errors and warnings

--- js/script.js
+++ js/script.js
@@ -1,48 +1,38 @@
-"use strict";
-
+//raw js
+var findAndReplace = function(toSearch, find, replaceWith, matchAnyCase) {
+  "use strict";
+  var regexOptions = "g";
+  if (matchAnyCase) { regexOptions += "i"; }
+  var regex = new RegExp("\\b" + find + "\\b", regexOptions);
+  var result = toSearch.replace(regex, replaceWith);
+  return result;
+};
 //jQuery
 $( document ).ready(function() {
+  "use strict";
     console.log( "jQuery Ready" );
-
     $("#findAndReplace").submit(function(event) {
       event.preventDefault();
-      var ignoreCase = $("input#ignoreCase").is(':checked');
+      var ignoreCase = $("input#ignoreCase").is(":checked");
       var textToSearch = $("textarea#toSearch").val();
       var find = $("input#find").val();
       var replaceWith = $("input#replaceWith").val();
-
       if (!textToSearch) {
         alert("No text to search!");
         return;
       }
-
       if (!find) {
         alert("No word to match!");
         return;
       }
-
       var result = findAndReplace(textToSearch, find, replaceWith, ignoreCase);
-
       $("div#result").text(result);
-
       var inputPanel = $("div#input-panel");
       var resultPanel = $("div#result-panel");
       //resize the result panel to match the input panel
       resultPanel.width(inputPanel.width());
       resultPanel.height(inputPanel.height());
       //unhide the panel
-      $("div#result-panel").css('visibility','visible');
-
-
+      $("div#result-panel").css("visibility", "visible");
     });
-
 });
-
-//raw js
-var findAndReplace = function(toSearch, find, replaceWith, matchAnyCase) {
-  var regexOptions = 'g'
-  if (matchAnyCase) { regexOptions += 'i'}
-  var regex = new RegExp('\\b' + find + '\\b',regexOptions);
-  var result = toSearch.replace(regex, replaceWith)
-  return result;
-};
